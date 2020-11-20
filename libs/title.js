8d76f27a593d5bc74ed39cd0d575018eeffa7d39
Paul Adams <paadams@deloitte.com>
2020-11-21 00:07:23 +0800

    testing jsdoc

--- libs/title.js
+++ libs/title.js
@@ -1,4 +1,11 @@
 const assert = require("assert").strict;
+/**
+ * Returns the Title of the wordpress site
+ * @name title
+ * @returns {string} returns a string representation of the title
+ * @example
+ *  title // => 'Wordpress Title'
+ */
 module.exports = ({ title }) => {
   assert(title._text, "Title does not exist");
   return title._text;
--- libs/version.js
+++ libs/version.js
@@ -1,6 +1,12 @@
 const SUPPORTED_VERSIONS = ["1.2"];
 const assert = require("assert").strict;
-
+/**
+ * Returns the WXR version used to export from the wordpress site
+ * @name title
+ * @returns {string} returns a string with the WXR version number
+ * @example
+ *  version // => '1.2'
+ */
 module.exports = ({ "wp:wxr_version": version }) => {
   let isSupported = false;
   if (SUPPORTED_VERSIONS.includes(version._text)) {
